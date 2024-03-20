c4dd5d70d10a7b5bbbac9daf1edfc3fa77eed2b2
Gabriel Orie <oriegabriel16@gmail.com>
2024-03-20 01:24:12 +0100

    Project Init Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        background: "var(--background)",
+        transparent: "transparent",
+        primary: {
+          DEFAULT: "var(--primary)",
+          500: "var(--primary)",
+        },
+      },
     },
   },
   plugins: [],
